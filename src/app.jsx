1701b0b2616c971dc0af273c7c6b7e1a8fbb5234
codrin-iftimie <iceboxic@gmail.com>
2014-09-07 15:13:26 +0300

    Proper React hook when creating markup
    TODO: updateComponent

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,121 @@
+var ReactCompositeComponentBase = require('react/lib/ReactCompositeComponent').Base
+var mountComponent = ReactCompositeComponentBase.prototype.mountComponent;
+ReactCompositeComponentBase.prototype.mountComponent = function(){
+  if(this._descriptor.props._parent){
+    arguments[1].parent = this._descriptor.props._parent
+  }
+  return mountComponent.apply(this, arguments);
+}
+
+var updateComponent = ReactCompositeComponentBase.prototype.updateComponent;
+ReactCompositeComponentBase.prototype.updateComponent = function(){
+  this.cssUpdate = true;
+  console.log(arguments)
+  return updateComponent.apply(this, arguments);
+  this.cssUpdate = true;
+}
+
+
+var ReactMultiChild = require('react/lib/ReactMultiChild');
+
+var mountChildren = ReactMultiChild.Mixin.mountChildren;
+
+var CssHelper = {
+  _getStyle: function(css){
+    // return;
+    return ComputeCSS(css, allDeclarations, allInharitances);
+  },
+  _getClassName: function(node, parent){
+    var stop = false;
+    if(parent){
+      if(!node.tagName && !node.cssUpdate){
+        node.props._parent = parent;
+        return;
+      }
+      var existingClasses = parent.props.parentClassName;
+      var allClasses = [];
+      for (var i = 0; i < existingClasses.length; i++) {
+        allClasses.push(existingClasses[i])
+      };
+      if(node.props.className){
+        allClasses.push('.' + node.props.className);
+      } else {
+        allClasses.push(node.tagName);
+      }
+      node.props.parentClassName = allClasses;
+    } else {
+      //root node
+      node.props.parentClassName = ['.' + node.props.className];
+    }
+    node.props.style = CssHelper._getStyle(node.props.parentClassName);
+  },
+  _getFirstChild: function(node, startFrom){
+    startFrom = startFrom || 0;
+    var kids = node.props.children;
+ 
+    if(!kids || startFrom === kids.length) {
+      return ;
+    }
+
+    for (var i = startFrom; i < kids.length; i++) {
+      var kid = kids[i];
+      if(typeof kid === 'object'){
+        return [i, kid];
+      }
+    };
+  },
+  _walkTheDom: function(node, parent){
+    var parNode = node;
+    CssHelper._getClassName(node, parent);
+    var lastChild = 0;
+    var firstNode = CssHelper._getFirstChild(node);
+    if(firstNode){
+      lastChild = firstNode[0] + 1;
+      node = firstNode[1];
+    } else {
+      node = null;
+    }
+    while(node){
+      CssHelper._walkTheDom(node, parNode);
+      var nextNode = CssHelper._getFirstChild(parNode, lastChild);
+      if(nextNode){
+        node = nextNode[1];
+        lastChild = nextNode[0] + 1;
+      } else {
+        node = null;
+      }
+    }
+  }
+}
+var once = false;
+ReactMultiChild.Mixin.mountChildren = function(){
+  var out = mountChildren.apply(this, arguments);
+  return out;
+};
+
+var ReactDOMComponent = require('react/lib/ReactDOMComponent');
+var _createOpenTagMarkupAndPutListeners = ReactDOMComponent.prototype._createOpenTagMarkupAndPutListeners;
+
+ReactDOMComponent.prototype._createOpenTagMarkupAndPutListeners = function(){
+  if(!once){
+    CssHelper._walkTheDom(this);
+    once = true;
+  }
+  if(arguments[0].parent){
+    CssHelper._walkTheDom(this, arguments[0].parent);
+  }
+
+  var out = _createOpenTagMarkupAndPutListeners.apply(this, arguments);
+  return out;
+}
+
+var _createContentMarkup = ReactDOMComponent.prototype._createContentMarkup;
+
+ReactDOMComponent.prototype._createContentMarkup = function(){
+  var out = _createContentMarkup.apply(this, arguments);
+  return out;
+}
+
 /** @jsx React.DOM */
 var React = require("react/addons");
 var ComputeCSS = require("./resolveStyle.js");
@@ -5,20 +123,13 @@ var CSS = require('../styles/main.css.js');
 var allDeclarations = CSS.allDeclarations;
 var allInharitances = CSS.allInharitances;
 var classSet = React.addons.classSet;
-var ReactOwner = require('react/lib/ReactOwner');
+
+
 
 var ComputeMixin = {
-  getInitialState: function(){
-    return {
-      force: false
-    }
-  },
   componentDidMount: function(){
   },
   componentDidUpdate: function(){
-    if(this._owner){
-      this._owner.updateStyle();
-    }
   }
 }
 
@@ -30,6 +141,7 @@ var hook = function(React){
     reactClass.mixins = mixins;
     return createClass(reactClass);
   }
+  console.log(React)
 };
 
 hook(React);
@@ -53,94 +165,6 @@ var ChildComp = React.createClass({
 	}
 });
 var App = React.createClass({
-	_getStyle: function(css){
-		// return;
-		return ComputeCSS(css, allDeclarations, allInharitances);
-	},
-	 _getClassName: function(node, parent){
-
-    if(!node.tagName){
-    	this._subRender(node, parent);
-    	return;
-    }
-
-    if(parent){
-    	var existingClasses = parent.props.parentClassName;
-    	var allClasses = [];
-    	for (var i = 0; i < existingClasses.length; i++) {
-    		allClasses.push(existingClasses[i])
-    	};
-	    allClasses.push('.' + node.props.className);
-	    node.props.parentClassName = allClasses;
-    } else {
-    	//root node
-    	node.props.parentClassName = ['.' + node.props.className];
-    }
-    node.props.style = this._getStyle(node.props.parentClassName);
-  },
-  _getFirstChild: function(node, startFrom){
-    startFrom = startFrom || 0;
-    var kids = node.props.children;
-    
-    if(kids && typeof kids !== 'string' && kids.render){
-      this._subRender(kids, node);
-    }
- 
- 
-    if(!kids || startFrom === kids.length) {
-      return ;
-    }
-
-    for (var i = startFrom; i < kids.length; i++) {
-      var kid = kids[i];
-      if(typeof kid === 'object'){
-        return [i, kid];
-      }
-    };
-  },
-  _walkTheDom: function(node, parent){
-    var parNode = node;
-    this._getClassName(node, parent);
-    var lastChild = 0;
-    var firstNode = this._getFirstChild(node);
-    if(firstNode){
-      lastChild = firstNode[0] + 1;
-      node = firstNode[1];
-    } else {
-      node = null;
-    }
-    while(node){
-      if(node.props.ref){
-        node._owner = this;
-        node._pendingOwner = this;
-      }
-      this._walkTheDom(node, parNode);
-      var nextNode = this._getFirstChild(parNode, lastChild);
-      if(nextNode){
-        node = nextNode[1];
-        lastChild = nextNode[0] + 1;
-      } else {
-        node = null;
-      }
-    }
-  },
-  _subRender: function(comp, parent){
-    if(comp.getInitialState){
-      comp.state = comp.getInitialState();
-    }
-    var orignalRender = comp._render || comp.render;
-    var render = comp._render ? comp._render() : comp.render();
-    this._walkTheDom(render, parent);
-    comp._render = orignalRender;
-    comp.render = function(){ return render };
-  },
-  updateStyle: function(){
-    this._subRender(this, null);
-    this.forceUpdate();
-  },
-  componentDidMount: function(){
-    this.updateStyle();
-	},
   _handleButtonClick: function(){
     this.refs['first-child-comp'].setState({
       test: true
